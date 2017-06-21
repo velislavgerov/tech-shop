81d72f59a27305ae9002bc2dee9bf3606736f994
thunderstruck47 <velislav.gerov.12@aberdeen.ac.uk>
2017-06-21 11:33:23 +0300

    users model

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # Local import
 from instance.config import app_config
-#from app import models
 
 db = SQLAlchemy()
-
-from app import models
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -16,8 +15,12 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login" # TODO
+        
     @app.route('/')
     def hello_world():
-        return 'Hello, World!\r\n{}x'.format(str(models.Test.get_all()))
-
+        return 'Hello, World!'
+    
     return app
--- app/models.py
+++ app/models.py
@@ -1,13 +1,40 @@
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class Test(db.Model):
-    __tablename__ = 'test'
+from app import db, login_manager
 
-    test = db.Column(db.Text, primary_key=True)
-    
-    @staticmethod
-    def get_all():
-        return Test.query.all()
+class User(UserMixin, db.Model):
+    """Crete an User table."""
+
+    # Ensures that the table will be named in plural
+    __tablename__  = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    password_hash = db.Column(db.String(128))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """Prevent password from beign accessed"""
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """Hash an set password."""
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """Check if password matches actual password"""
+        return check_password_hash(password)
 
     def __repr__(self):
-        return '<Test: {}>'.format(self.test)
+        return '<User: {} {}'.format(self.first_name, self.last_name)
+
+    # Set up user_loader
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
