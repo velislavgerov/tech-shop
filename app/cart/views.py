21ecd1336dae70baf7281f8bcdfabebb056be605
thunderstruck47 <velislav.gerov.12@aberdeen.ac.uk>
2017-06-23 17:29:09 +0300

    adding multiple items to cart, depending on availability

--- app/cart/views.py
+++ app/cart/views.py
@@ -13,11 +13,13 @@ def list_items():
     List all items in cart
     """
     product_ids = Cart.query.filter_by(user_id = current_user.id).all()
-    product_ids = [x.product_id for x in product_ids]
-    products = Product.query.filter(Product.id.in_(product_ids)).all()
+    product_ids = {x.product_id: x.quantity for x in product_ids}
+    products = Product.query.filter(Product.id.in_(list(product_ids.keys()))).all()
+    for x in products:
+        x.quantity = product_ids[x.id]
     total = None
     if products:
-        total = sum([x.price for x in products])
+        total = sum([x.price*x.quantity for x in products])
     return render_template('cart/cart.html', products=products, total=total, title="Cart")
 
 @cart.route('/cart/add/<int:id>', methods=['GET','POST'])
@@ -28,14 +30,22 @@ def add_to_cart(id):
     """
     # XXX:check_admin - should they have a cart?
     # XXX: product_id should be checked
-    cart = Cart(user_id=current_user.id,
+    item = Cart.query.filter_by(user_id=current_user.id).filter_by(product_id=id).first()
+    if item: # was found
+        product = Product.query.filter_by(id=item.product_id).first()
+        if item.quantity + 1 > product.quantity:
+            flash('You can not add more of this item.')
+        else:
+            item.quantity += 1
+            db.session.merge(item)
+            db.session.commit()
+            flash('Item updated.')
+    else:
+        item = Cart(user_id=current_user.id,
                 product_id=id)
-    try:
-        db.session.merge(cart)
+        db.session.add(item)
         db.session.commit()
         flash('Item added.')
-    except:
-        flash('Item already in your shopping cart.')
 
     return redirect(url_for('home.homepage'))
 
