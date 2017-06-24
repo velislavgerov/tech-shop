5852a88da8484bd51694701a158b57ced0fd3cac
thunderstruck47 <velislav.gerov.12@aberdeen.ac.uk>
2017-06-24 16:08:18 +0300

    [order] Order confirmation page

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,8 @@
 from flask import abort, render_template, request
 from flask_login import current_user, login_required
 
-from ..models import Product, Category
+from ..models import Product, Category, Cart, Address
+from ..auth.forms import AccountForm
 
 from . import home
 
@@ -38,8 +39,49 @@ def admin_dashboard():
     return render_template('home/admin_dashboard.html', title="Dashboard")
 
 @home.route('/order')
+@login_required
 def order():
     """
     Render the order page
     """
-    return render_template('home/order.html', title="Order")
+    items = Cart.query.filter_by(user_id=current_user.id).all()
+    product_ids = {x.product_id: x.quantity for x in items}
+    products = Product.query.filter(Product.id.in_(list(product_ids.keys()))).all()
+    for x in products:
+        x.quantity = product_ids[x.id]
+    total = None
+    if products:
+        total = sum([x.price*x.quantity for x in products])
+
+    form = AccountForm()
+    address = Address.query.filter_by(user_id=current_user.id).first()
+    if form.validate_on_submit():
+        if not address:
+            address = Address(user_id=current_user.id)
+        address.tel_number = form.tel_number.data
+        address.address_line_1 = form.address_line_1.data
+        address.address_line_2 = form.address_line_2.data
+        address.city = form.city.data
+        address.county = form.county.data
+        address.postcode = form.postcode.data
+        address.country = form.country.data
+        try:
+            db.session.merge(address)
+            db.session.commit()
+            flash('You have succesfully updated your details.')
+        except:
+            flash('Sorry, you can\'t do this right now.')
+    
+    form.email.data = current_user.email
+    form.first_name.data = current_user.first_name
+    form.last_name.data = current_user.last_name
+    if address:
+        form.tel_number.data = address.tel_number
+        form.address_line_1.data = address.address_line_1
+        form.address_line_2.data = address.address_line_2
+        form.city.data = address.city
+        form.county.data = address.county
+        form.postcode.data = address.postcode
+        form.country.data = address.country
+
+    return render_template('home/order.html', products=products, form=form, total=total, title="Order")
